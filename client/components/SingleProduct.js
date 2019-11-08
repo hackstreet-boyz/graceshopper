d846eced192c4a7f01530bac199a8cc6287c358e
Jake Rothenberg <jrothenberg125@gmail.com>
2019-11-08 14:23:25 -0500

    guest cart completed for singleProduct

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -8,6 +8,7 @@ class SingleProduct extends React.Component {
   constructor() {
     super()
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.guestCartAdd = this.guestCartAdd.bind(this)
   }
   componentDidMount() {
     this.props.getSingleProduct(this.props.match.params.productId)
@@ -26,6 +27,27 @@ class SingleProduct extends React.Component {
     this.props.addItemToCart(this.props.userId, putInCart)
   }
 
+  guestCartAdd() {
+    const guestCart = 'guestCart'
+    let currentGuestCart = JSON.parse(window.localStorage.getItem(guestCart))
+    if (currentGuestCart && currentGuestCart[this.props.singleProduct.id]) {
+      currentGuestCart[
+        this.props.singleProduct.id
+      ].quantity = ++currentGuestCart[this.props.singleProduct.id].quantity
+      window.localStorage.setItem(guestCart, JSON.stringify(currentGuestCart))
+    } else if (currentGuestCart) {
+      currentGuestCart[this.props.singleProduct.id] = this.props.singleProduct
+      currentGuestCart[this.props.singleProduct.id].quantity = 1
+      window.localStorage.setItem(guestCart, JSON.stringify(currentGuestCart))
+    } else {
+      const initalCart = {
+        [this.props.singleProduct.id]: this.props.singleProduct
+      }
+      initalCart[this.props.singleProduct.id].quantity = 1
+      window.localStorage.setItem(guestCart, JSON.stringify(initalCart))
+    }
+  }
+
   render() {
     const {
       name,
@@ -37,7 +59,7 @@ class SingleProduct extends React.Component {
       category,
       stock
     } = this.props.singleProduct
-    console.log('THIS IS SINGLE PRODUCT', this.props.singleProduct)
+
     return this.props.singleProduct ? (
       <div>
         <h1>{name}</h1>
@@ -46,7 +68,12 @@ class SingleProduct extends React.Component {
         <p>{brand}</p>
         <p>{description}</p>
         <p>{category}</p>
-        <button type="button" onClick={this.handleSubmit}>
+        <button
+          type="button"
+          onClick={
+            this.props.isLoggedIn ? this.handleSubmit : this.guestCartAdd
+          }
+        >
           ADD ITEM
         </button>
       </div>
@@ -58,7 +85,8 @@ const mapStateToProps = state => {
   return {
     userId: state.user.id,
     singleProduct: state.products.singleProduct,
-    state: state
+    state: state,
+    isLoggedIn: !!state.user.id
   }
 }
 
