224df29164f33be15d1039c10a0214859c51983a
Jake Rothenberg <jrothenberg125@gmail.com>
2019-11-12 19:54:32 -0500

    added guest cart thing

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -46,10 +46,15 @@ class Cart extends React.Component {
   }
 
   handleSubmit(event) {
-    event.preventDefault()
-    console.log('props:', this.props.cart)
-    this.props.submitOrder(this.props.user)
-    this.redirectFunc()
+    if (this.props.user.id) {
+      event.preventDefault()
+      console.log('props:', this.props.cart)
+      this.props.submitOrder(this.props.user)
+      this.redirectFunc()
+    } else {
+      window.localStorage.clear()
+      this.redirectFunc()
+    }
   }
 
   increase(product) {
